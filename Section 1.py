304a1e5c7c111f36773a0baea7ed0389e0a97750
Gheibihayat <gheibi65@yahoo.com>
2024-11-03 02:43:24 +0330

    First commit - uploading my code

---
+++ Section 1.py
@@ -0,0 +1,75 @@
+print(21)
+print(20)
+print(27)
+
+# Variable
+# in jhdjhcbcgh
+
+#int
+gene_expression1 = 6
+print(type(gene_expression1))
+
+
+# float
+gene_expression2 = 4.7
+print(gene_expression2)
+print(type(gene_expression2))
+
+# str
+gene_name1 = "Psdkjhlbs   df kjshdbckjh ef444323  ljsdbcl  erg lkjshdCKS9"
+gene_name2 = 'PCK'
+print(gene_name1)
+
+print(type(gene_name1))
+
+# Bool
+patient1 = True
+patient2 = False
+
+print(type(patient2))
+
+print(patient1)
+
+# name of Variable
+
+# Why variable?
+
+# method 1
+print((3+6+9+1)/4)
+
+# method 2
+
+gene_expression1 = 3
+gene_expression2 = 6
+gene_expression3 = 9
+gene_expression4 = 1
+mean_Gene_Expresion = (gene_expression1+gene_expression2+gene_expression3+gene_expression4)/4
+print(mean_Gene_Expresion)
+gene_expression1 = 8.1
+mean_Gene_Expresion = (gene_expression1+gene_expression2+gene_expression3+gene_expression4)/4
+print(mean_Gene_Expresion)
+
+
+# readability
+# reusebility
+
+# RAM
+# HARD
+# 1 B   1000B = 1KB   1000KB = 1MB    1000MB = 1 GB
+# 1 GB = 1000000000
+# 4 GB = 4000000000
+# 8 16 32
+
+print(gene_expression1+gene_expression2)
+print(gene_expression1/gene_expression2)
+print(gene_expression1*gene_expression2)
+print(gene_expression1-gene_expression2)
+
+print(gene_name1+gene_name2)
+
+
+
+P54 = 3.99
+PCKS9 = 8.22
+CD47 = 2.52
+mean_Gene_Expresion = (P54 + PCKS9 + CD47)/3
